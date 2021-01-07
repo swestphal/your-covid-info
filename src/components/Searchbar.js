bf7a114f1bf0d9a068222f9a6bce760cba32ae2e
Simone Westphal <sw@simonewestphal.de>
2021-01-07 10:04:12 +0100

    refactor: Add highlight searchpart in autosuggest output

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Searchbar = (props) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState(props.countries);
 
+  useEffect(() => {
+    getSuggestions();
+  }, [searchTerm]);
+
   // https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
   function escapeRegexCharacters(str) {
     return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
   }
+
+  const wrapSearchTerm = (country, term) => {
+    return (
+      <span>
+        {country.split(term).reduce((prev, current, foundPos) => {
+          if (!foundPos) {
+            return [current];
+          }
+          return prev.concat(<b key={term + current}>{term}</b>, current);
+        }, [])}
+      </span>
+    );
+  };
 
   const getSuggestions = () => {
     if (searchTerm.length > 0) {
@@ -21,14 +38,19 @@ const Searchbar = (props) => {
   };
 
   const onChangeSearch = (e) => {
-    setSearchTerm(escapeRegexCharacters(e.target.value));
-    getSuggestions();
+    //setSearchTerm(escapeRegexCharacters(e.target.value));
+    const enteredValue = e.target.value;
+    const firstUpperValue =
+      enteredValue.charAt(0).toUpperCase() + enteredValue.slice(1);
+    //todo extract first upper function in utils
+    setSearchTerm(firstUpperValue);
+    console.log('onchange:', e.target.value);
   };
 
   const renderedResults = results.map((country) => (
     <div key={country.value} className="item">
       <div className="content">
-        <div className="header">{country.name}</div>
+        <div className="header">{wrapSearchTerm(country.name, searchTerm)}</div>
       </div>
     </div>
   ));
